6191511aa512e7e8c7ef906bac110e09a7670c48
Shantanu Motiani <shantanumotiani@gmail.com>
2022-09-29 16:27:40 +0200

    scanning planes into runway and hangar

--- airport/airport.c
+++ airport/airport.c
@@ -13,8 +13,22 @@ const int maxRunway = 7;
 int main(int argc, char *argv[]) {
   int n; // number of planes to read
   scanf("%d",&n);
+  
+  int* planeID;
+  char* needsRepairs[4];
 
-  // ...
+  Queue Runway = newQueue(maxRunway);
+  Stack Hangar = newStack(maxHangar);
+
+  for (int i=0; i<n; i++) {
+    scanf("%d %s", &planeID, &needsRepairs);
+    if (*needsRepairs = "no") {
+      enqueue (planeID, &Runway);
+    }
+    else {
+      push(planeID, &Hangar);
+    }
+  }
 
   return 0;
 }
