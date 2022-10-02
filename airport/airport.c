e21c11b5106c935cb010100765cb7d6b199faa69
Shantanu Motiani <shantanumotiani@gmail.com>
2022-10-02 17:38:30 +0200

    Segmentation fault

--- airport/airport.c
+++ airport/airport.c
@@ -14,27 +14,27 @@ int main(int argc, char *argv[]) {
   int n; // number of planes to read
   printf("Asking for n\n"); //remove this later
   scanf("%d",&n);
-  
+
+  Queue Runway = newQueue(maxRunway);
+  Stack Hangar = newStack(maxHangar);
+
   int planeID;
   char *needsrepairs;
   int hangarcount = 0;
   int runwaycount = 0;
 
-  Queue Runway = newQueue(maxRunway);
-  Stack Hangar = newStack(maxHangar);
-
   printf("Asking for planes\n"); //remove this later
-  for (int i = 0; i < n; i++) {
-    scanf("%d, %s\n", &planeID, needsrepairs);
+  for (int i=0; i<n; i++) {
+    scanf("%d %s", &planeID, needsrepairs);
     if (strcmp(needsrepairs, "yes") == 0) { //Moving planes into Hangar
       push (planeID, &Hangar);
       hangarcount++;
       if (hangarcount == maxHangar) { //When hangar is full
         printf("Ready for takeoff!\n");
-        for (i = 0; i < runwaycount; i++) { //Clearing Runway
+        for (i=0; i<runwaycount; i++) { //Clearing Runway
           dequeue (&Runway);
         } 
-        for (i = 0; i < maxHangar; i++) { //Moving planes from Hangar to Runway
+        for (i=0; i<maxHangar; i++) { //Moving planes from Hangar to Runway
           enqueue (pop (&Hangar), &Runway);
         }
       }
@@ -44,7 +44,7 @@ int main(int argc, char *argv[]) {
       runwaycount++;
       if (runwaycount == maxRunway) { //Clearing runway when runway is full
         printf("Ready for takeoff!\n");
-        for (i = 0; i < runwaycount; i++) {
+        for (i=0; i<runwaycount; i++) {
           dequeue (&Runway);    
         }
       }
